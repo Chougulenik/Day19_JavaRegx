eb7e873f0bdd6da2743dc5d882de3c2203d6327c
Chougulenik <nikitachougule47@gmail.com>
2022-03-29 14:01:23 -0400

    added

--- src/com/bridgelabz/RegularExpression.java
+++ src/com/bridgelabz/RegularExpression.java
@@ -10,14 +10,24 @@ public class RegularExpression {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your first name :");
 		String firstName = scan.nextLine();
+		String lastName = scan.nextLine();
 		
 		System.out.println("first name is : " +firstName);
 		System.out.println("first name is valid ? " + firstName(firstName));
+		
+		System.out.println("first name is : " +lastName);
+		System.out.println("first name is valid ? " + lastName(lastName));
 		
 	}
 	
 	private static boolean firstName(String firstName) {
 		
-		return firstName.matches("[A-Z]{1}[a-z]{3,}");	
+		return firstName.matches("[A-Z]{1}[a-z]{3}");	
 	}
+	
+	private static boolean lastName(String lastName) {
+		
+		return lastName.matches("[A-Z]{1}[a-z]{3}");
+	}
+
 }
